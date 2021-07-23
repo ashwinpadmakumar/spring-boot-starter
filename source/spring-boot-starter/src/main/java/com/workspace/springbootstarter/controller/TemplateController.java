2317cda7f7fd88a13badbddf2028ee40c7e70a13
Ashwin Padmakumar <ashwinpadmakumar@gmail.com>
2021-07-23 13:58:14 +0530

    Init starter project

---
+++ source/spring-boot-starter/src/main/java/com/workspace/springbootstarter/controller/TemplateController.java
@@ -0,0 +1,27 @@
+/**
+ * Description: Template Controller.
+ *
+ * @author: Ashwin Padmakumar
+ * @since: 23/07/21
+ * @version: 0.1
+ */
+
+package com.workspace.springbootstarter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/noop")
+@Slf4j
+public class TemplateController {
+
+  @GetMapping
+  public ResponseEntity<String> noop() {
+    return new ResponseEntity<>("No Operation", HttpStatus.OK);
+  }
+}
---
+++ source/spring-boot-starter/src/test/java/com/workspace/springbootstarter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspace.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

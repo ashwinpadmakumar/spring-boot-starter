c188f6568f65eeb2d25d3c50d400d682f4f76cf9
Ashwin Padmakumar <ashwinpadmakumar@gmail.com>
2024-11-30 21:03:19 +0530

    Fixig build

--- source/spring-boot-template/service/src/test/java/com/workspace/service/ServiceApplicationTests.java
+++ source/spring-boot-template/service/src/test/java/com/workspace/service/ServiceApplicationTests.java
@@ -8,13 +8,12 @@
 
 package com.workspace.service;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ServiceApplicationTests {

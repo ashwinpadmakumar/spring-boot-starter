8adcd90e984754680b68d6cf43c2899137fcd8cf
Ashwin Padmakumar <ashwinpadmakumar@gmail.com>
2024-12-07 16:45:22 +0530

    Fixing Jenkinsfile

--- source/spring-boot-template/Jenkinsfile.groovy
+++ source/spring-boot-template/Jenkinsfile.groovy
@@ -24,24 +24,24 @@ pipeline {
                         // Update package information and install Docker
                         sh '''
                         # Update package information
-                        sudo apt-get update
+                        apt-get update
 
                         # Install Docker dependencies
-                        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+                        apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
                         # Add Docker's official GPG key
                         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-                        sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+                        gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
                         # Add Docker's official APT repository
                         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] " + \\
                         https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
                         # Update the package database
-                        sudo apt-get update
+                        apt-get update
 
                         # Install Docker CE
-                        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+                        apt-get install -y docker-ce docker-ce-cli containerd.io
                         '''
 
                         echo 'Docker installation completed.'
